c4559257baa4d117d414f088c2b149bea8619c8c
Vageesh-Jayaraman <143870355+Vageesh-Jayaraman@users.noreply.github.com>
2024-06-21 17:33:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+import streamlit as st
+import gdown
+import os
 import cv2
 import numpy as np
 import tensorflow as tf
-import streamlit as st
 from PIL import Image
 
-model = tf.keras.models.load_model('floodSegmentationModel.h5')
+@st.cache_data
+def download_model():
+    url = 'https://drive.google.com/uc?id=1oCZFRhuZ9j6t-FdPpg1E6SUX_XkJ4nRW'
+    output = 'floodSegmentationModel.h5'
+    if not os.path.exists(output):
+        gdown.download(url, output, quiet=False)
+    return output
 
-def segment_image(image):
+@st.cache_resource
+def load_model():
+    model_path = download_model()
+    model = tf.keras.models.load_model(model_path)
+    return model
+
+def segment_image(image, model):
     input_image = cv2.resize(image, (128, 128))
-    input_image = np.expand_dims(input_image, axis=0)  
-    input_image = input_image / 255.0  
+    input_image = np.expand_dims(input_image, axis=0)
+    input_image = input_image / 255.0
 
     segmentation_mask = model.predict(input_image)
-    
-    segmentation_mask = segmentation_mask[0]  
-    segmentation_mask = cv2.resize(segmentation_mask, (image.shape[1], image.shape[0]))  # Resize to match image dimensions
-    
+    segmentation_mask = segmentation_mask[0]
+    segmentation_mask = cv2.resize(segmentation_mask, (image.shape[1], image.shape[0]))
     segmentation_mask = (segmentation_mask > 0.5).astype(np.uint8)
 
     return segmentation_mask
@@ -29,15 +41,18 @@ def calculate_water_coverage(mask):
 def main():
     st.title('Flood Image Segmentation')
     st.markdown("[![GitHub](https://img.shields.io/badge/GitHub-000000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/Vageesh-Jayaraman/Unet-Based-Flood-Image-Segmentation-in-TensorFlow/tree/main)")
+    st.markdown("[![Google Drive](https://img.shields.io/badge/Download%20Model-Google%20Drive-blue?style=for-the-badge&logo=googledrive&logoColor=white)](https://drive.google.com/file/d/1oCZFRhuZ9j6t-FdPpg1E6SUX_XkJ4nRW/view?usp=sharing)")
 
     uploaded_file = st.file_uploader('Upload an image file', type=['jpg', 'jpeg', 'png'])
 
     if uploaded_file is not None:
+        with st.spinner('Downloading and loading the model...'):
+            model = load_model()
+
         image = Image.open(uploaded_file)
         image_np = np.array(image)
 
-        segmented_mask = segment_image(image_np)
-
+        segmented_mask = segment_image(image_np, model)
         water_coverage = calculate_water_coverage(segmented_mask)
 
         colored_mask = np.zeros_like(image_np)
@@ -59,7 +74,6 @@ def main():
 
     st.subheader('Tech Stack Used')
     tech_stack_images = ['icons/tensorflow.png', 'icons/opencv.png', 'icons/streamlit.png']
-
     st.image(tech_stack_images, width=60)
 
 if __name__ == "__main__":
